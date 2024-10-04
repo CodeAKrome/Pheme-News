17d0487172be94fc169cada6b1f78f961d06d3b8
kyle gaspar <kyleg.job@gmail.com>
2024-10-04 01:51:22 -0400

    new flesh

--- src/nerd.py
+++ src/nerd.py
@@ -7,14 +7,9 @@ import os
 from litellm import completion
 from time import perf_counter
 import re
-from groq import Groq
+#from groq import Groq
 from functools import cache
 from time import sleep
-
-
-# from colorama import init, Fore, Back, Style
-# init()
-
 from lib.ollamaai import OllamaAI
 
 """model name, [groq ollama] default ollama"""
@@ -26,115 +21,113 @@ from lib.ollamaai import OllamaAI
 # MODEL = "gemini/gemini-1.5-flash-exp-0827"
 # API_KEY=os.environ.get('GEMINI_API_KEY')
 
-MODEL = "bespoke-minicheck"
-MODEL = "nemotron-mini"
-MODEL = "solar-pro"
-MODEL = "mistral-nemo"
-MODEL = "hermes3:70b"
-MODEL = "qwen2.5:72b"
-MODEL = "gemma:7b"
-MODEL = "phi3:14b"
-MODEL = "phi3:medium-128k"
-MODEL = "gemma2:27b"
+# MODEL = "bespoke-minicheck"
+# MODEL = "nemotron-mini"
+# MODEL = "solar-pro"
+# MODEL = "mistral-nemo"
+# MODEL = "hermes3:70b"
+# MODEL = "qwen2.5:72b"
+# MODEL = "gemma:7b"
+# MODEL = "phi3:14b"
+# MODEL = "phi3:medium-128k"
+# MODEL = "gemma2:27b"
 MODEL = "llama3.1:70b"
 
+# PATTERN = re.compile(r"[^0-9]")
 
+# def numeric(text):
+#     return PATTERN.sub("", text)
 
-PATTERN = re.compile(r"[^0-9]")
-
-def numeric(text):
-    return PATTERN.sub("", text)
-
-def digits(string):
-    digits = ""
-    for char in string:
-        if char.isdigit():
-            digits += char
-        else:
-            break
+# def digits(string):
+#     digits = ""
+#     for char in string:
+#         if char.isdigit():
+#             digits += char
+#         else:
+#             break
     
-    sys.stderr.write(f"digits: {digits}\n")
+#     sys.stderr.write(f"digits: {digits}\n")
     
-    return digits
+#     return digits
 
 
 # ------
 
-class GroqAI:
-    def __init__(self, system_prompt = None, model="llama3-70b-8192", max_tokens=1500, temperature=.1, api_key=None):
-        self.api_key = api_key or os.environ.get("GROQ_API_KEY")
-        self.groq_client = Groq(api_key=self.api_key)
-        self.model = model
-        self.max_tokens = max_tokens
-        self.temperature = temperature
-        self.set_system(system_prompt)
+# class GroqAI:
+#     def __init__(self, system_prompt = None, model="llama3-70b-8192", max_tokens=1500, temperature=.1, api_key=None):
+#         self.api_key = api_key or os.environ.get("GROQ_API_KEY")
+#         self.groq_client = Groq(api_key=self.api_key)
+#         self.model = model
+#         self.max_tokens = max_tokens
+#         self.temperature = temperature
+#         self.set_system(system_prompt)
 
-    def set_system(self, prompt):
-        self.prompt = prompt or "You are a helpful assistant. You reply with short, accurate answers."
-        self.system_prompt = {"role": "system", "content": self.prompt}
-        self.chat_history = [self.system_prompt]
+#     def set_system(self, prompt):
+#         self.prompt = prompt or "You are a helpful assistant. You reply with short, accurate answers."
+#         self.system_prompt = {"role": "system", "content": self.prompt}
+#         self.chat_history = [self.system_prompt]
 
-    def says(self, prompt):
-        self.chat_history.append({"role": "user", "content": prompt})
-        #sys.stderr.write(f"history: {self.chat_history}\n")
-        response = self.groq_client.chat.completions.create(
-            model=self.model,
-            messages=self.chat_history,
-            max_tokens=self.max_tokens,
-            temperature=self.temperature
-        )
-        self.chat_history.append({
-            "role": "assistant",
-            "content": response.choices[0].message.content
-        })
-        return response.choices[0].message.content
+#     def says(self, prompt):
+#         self.chat_history.append({"role": "user", "content": prompt})
+#         #sys.stderr.write(f"history: {self.chat_history}\n")
+#         response = self.groq_client.chat.completions.create(
+#             model=self.model,
+#             messages=self.chat_history,
+#             max_tokens=self.max_tokens,
+#             temperature=self.temperature
+#         )
+#         self.chat_history.append({
+#             "role": "assistant",
+#             "content": response.choices[0].message.content
+#         })
+#         return response.choices[0].message.content
 
 
 # -----    
 
-def gemini_says(system, prompt):
-    system = system.replace("\n", " ")
+# def gemini_says(system, prompt):
+#     system = system.replace("\n", " ")
 
-    response = completion(
-        model=MODEL,
-        messages=[{"role":"system", "content": system}, {"role": "user", "content": prompt}]
-    )
-    return response.choices[0].message.content
+#     response = completion(
+#         model=MODEL,
+#         messages=[{"role":"system", "content": system}, {"role": "user", "content": prompt}]
+#     )
+#     return response.choices[0].message.content
 
 
-def wikipeeps00(suspect, max_results=10):
-    try:
-        # Search for the suspect
-        search_results = wiki.search(suspect, results=max_results)
+# def wikipeeps00(suspect, max_results=10):
+#     try:
+#         # Search for the suspect
+#         search_results = wiki.search(suspect, results=max_results)
         
-        people = []
-        for result in search_results:
-            try:
-                # Get the page for each result
-                page = wiki.page(result, auto_suggest=False)
+#         people = []
+#         for result in search_results:
+#             try:
+#                 # Get the page for each result
+#                 page = wiki.page(result, auto_suggest=False)
                 
-                # Check if the page is about a person
-                if any(category.lower().endswith('births') for category in page.categories):
-                    people.append(page.title)
-            except (DisambiguationError, PageError):
-                # Skip disambiguation pages and non-existent pages
-                continue
+#                 # Check if the page is about a person
+#                 if any(category.lower().endswith('births') for category in page.categories):
+#                     people.append(page.title)
+#             except (DisambiguationError, PageError):
+#                 # Skip disambiguation pages and non-existent pages
+#                 continue
         
-        return people
-    except Exception as e:
-        sys.stderr.write(f"An error occurred: {e}")
-        return []
+#         return people
+#     except Exception as e:
+#         sys.stderr.write(f"An error occurred: {e}")
+#         return []
 
-@cache
-def wikipeeps(suspect):
-    # Check if the page is about a person
-    try:
-        page = wiki.page(suspect, auto_suggest=False)
-        if any(category.lower().endswith('births') for category in page.categories):
-            return True
-    except Exception as e:
-        sys.stderr.write(f"An error occurred: {e}")
-    return False
+# @cache
+# def wikipeeps(suspect):
+#     # Check if the page is about a person
+#     try:
+#         page = wiki.page(suspect, auto_suggest=False)
+#         if any(category.lower().endswith('births') for category in page.categories):
+#             return True
+#     except Exception as e:
+#         sys.stderr.write(f"An error occurred: {e}")
+#     return False
 
 
 @cache
@@ -194,6 +187,156 @@ def nerd(suspect, sentence):
 
     return None
             
+@cache
+def nerd2(tag, suspect, sentence):
+    system_base = f"You are a world famous detective like Sherlock Holmes. You always answer accurately.\n"
+    system_base += """Tag	Meaning	Example
+EVENT	event name	Super Bowl, Olympics
+FAC	building name	Empire State Building, Eiffel Tower
+GPE	geo-political entity	United States, France
+LANGUAGE	language name	English, Spanish
+LAW	law name	Constitution, Copyright Act
+LOC	location name	New York City, Paris
+NORP	affiliation	Republican, Democrat
+ORG	organization name	NASA, Google
+PERSON	person name	John Doe, Jane Smith
+PRODUCT	product name	iPhone, MacBook
+WORK_OF_ART	name of work of art	Mona Lisa, Star Wars
+"""
+    if tag == "PERSON":
+        system_base += f"You will correctly identify this <{tag}>person</{tag}>. You will not mistake them for any other person or member of their family like a son, daughter or wife.\n"
+        system_base += "Professions, sexes and nationalities must match for correct identification.\n"
+    if tag == "EVENT":
+        system_base += f"You will correcty identify this <{tag}>event</{tag}>. You will match location time, and other details. You will not confuse it with any other event.\n"
+    if tag == "FAC":
+        system_base += f"You will correctly identify this <{tag}>facility or building</{tag}>. You will match the location and name. You will not confuse it with any other building or facility.\n"
+    if tag == "GPE":
+        system_base += f"You will correctly identify this <{tag}>geo-political entity</{tag}>, such as a political party or country.\n"
+    if tag == "LANGUAGE":
+        system_base += f"You will correctly identify this <{tag}>language</{tag}>. You will not mistake it for any other.\n"
+    if tag == "LAW":
+        system_base += f"You will correctly identify this <{tag}>law</{tag}>. You will match and legal references and statutes. You will not mistake it for any other.\n"
+    if tag == "LOC":
+        system_base += f"You will correctly identify this <{tag}>location</{tag}>. You will pay attention to country, state, city and other geographic details.\nYou will not mistake it for any other.\n"
+    if tag == "NORP":
+        system_base += f"You will correctly identify this <{tag}>affiliation</{tag}>. You will not mistake it for any other.\n"
+    if tag == "ORG":
+        system_base += f"You will correctly identify this <{tag}>organization</{tag}>. You will match name and type. You will not mistake it for any other.\n"
+    if tag == "PRODUCT":
+        system_base += f"You will correctly identify this <{tag}>product</{tag}>. You will identify it by brand and name. You will not mistake it for any other.\n"
+    if tag == "WORK_OF_ART":
+        system_base += f"You will correctly identify this <{tag}>work of art</{tag}>. You will match painter and subject. You will not mistake it for any other.\n"
+
+    system = f"{system_base}Only reply with 'yes' or 'no' to the following question:\n"
+
+    assistant.set_system(system)
+    usual_suspects = summ_people(suspect)
+    
+    for person, summ in usual_suspects.items():
+        prompt = f"Is <{tag}>{suspect}</{tag}> in the following example:\n<example>{sentence}</example>\nthe same as <{tag}>{person}</{tag}> in the following summary:\n<summary>{summ}</summary>\nAnswer 'yes' or 'no'.\n"
+        raw = assistant.says(prompt)
+
+        sys.stderr.write(f"RAW: {raw}\n")
+        
+        ans = "no"
+        if "yes" in raw.lower():
+            ans = "yes"
+
+        # sys.stderr.write(f"\033[41m\033[91m {suspect} -> {person}\t{ans} \033[0m\n")
+        sys.stderr.write(f"{suspect} -> {person}\t{ans}\n")
+                
+        if "yes" in ans:
+            sys.stderr.write(f"\n\n\033[41m\033[91m Match found: {suspect} -> {person}\033[0m\nsummary: {summ}\n\n")
+            return person
+
+    return None
+            
+
+def readstd(callback):
+    recno = 1
+    for line in sys.stdin:
+        sys.stderr.write(f"\n\n=== RECNO: {recno} ===\n\n")
+        recno += 1
+
+        line = line.strip()
+        if not line: continue
+        try:
+            data = loads(line)
+        except JSONDecodeError as e:
+            sys.stderr.write(f"JSON: {e}\n{line}\n")
+            continue
+        # This should mean this is an rss flavored record
+        if not 'ner' in data:
+            print(line)
+            continue
+        callback(data)
+
+def procart(data):
+    noproc=["MONEY", "ORDINAL", "PERCENT", "QUANTITY", "TIME", "DATE", "CARDINAL"]
+    ner = data['ner']
+    art_map = {}
+
+    # Pass 1
+    examples = {}
+    for ent in ner:
+        for span in ent['spans']:
+            # ORG/name or PERSON/name etc.
+            suspect = f"{span['value']}/{span['text']}"
+            sentence = ent['sentence']
+            if suspect in examples:
+                examples[suspect] += sentence
+            else:
+                examples[suspect] = sentence
+
+    wikimap = []
+    # PASS 2
+    for ent in ner:
+        start_time = perf_counter()
+
+        sys.stderr.write(f"\n=>\t{ent['sentence']}\n")
+
+        # NER to the D in single sentence
+        for span in ent['spans']:
+            # ORG/name or PERSON/name etc.
+            tag = span['value']
+            if tag in noproc: continue
+            suspect_key = f"{tag}/{span['text']}"
+            suspect = span['text']
+            sentence = ent['sentence']
+            res = None
+
+            sys.stderr.write(f"\nTRY: {suspect}\n")
+
+            for aka, entity in art_map.items():
+                if suspect in aka:
+                    res = art_map[aka]
+                else:
+                    if suspect in entity:
+                        res = art_map[entity]
+                if res:
+                    art_map[suspect_key] = entity
+
+                    sys.stderr.write(f"\n\033[41m\033[91m NERD {suspect} -> {res} added {suspect_key} -> {entity} \033[0m\n")
+
+                    break
+            if not res:
+                if suspect and suspect_key in examples:
+                    res = nerd2(tag, suspect, examples[suspect_key])
+            
+            if not res: continue
+            # resets every article
+            art_map[suspect_key] = res
+            art_map[res] = res
+
+        wikimap.append(art_map)
+    lap = perf_counter() - start_time
+    
+    sys.stderr.write(f"\nTime: {lap:.2f}s\n")
+    
+    data['wikimap'] = wikimap
+    print(dumps(data))
+    
+
             
 def main(assistant, nerd_map):
     recno = 1
@@ -220,7 +363,8 @@ def main(assistant, nerd_map):
         wikimap = []
         art_map = {}
 
-        # Collect example sentences.
+        # PASS 1
+        # Collect example sentences for PERSONs.
         examples = {}
         for ent in ner:
             for span in ent['spans']:
@@ -232,7 +376,7 @@ def main(assistant, nerd_map):
                     else:
                         examples[suspect] = sentence
 
-        # Article iteration, 2nd pass.
+        # PASS 2
         for ent in ner:
             start_time = perf_counter()
             sys.stderr.write(f"\n=>\t{ent['sentence']}\n")
@@ -256,7 +400,8 @@ def main(assistant, nerd_map):
                             sys.stderr.write(f"\n\033[41m\033[91m NERD {suspect} -> {res} added {suspect} -> {person} \033[0m\n")
                             break
                     if not res:
-                        res = nerd(suspect, examples[suspect])
+                        if suspect and examples[suspect]:
+                            res = nerd(suspect, examples[suspect])
                     
                     if not res: continue
                     # This will keep mapping, no harm
@@ -276,6 +421,8 @@ def main(assistant, nerd_map):
         data['wikimap'] = wikimap
         print(dumps(data))
 
+
+
 if __name__ == "__main__":
     run_start_time = perf_counter()
 
@@ -287,18 +434,23 @@ if __name__ == "__main__":
 
     assistant_name = "Ollama"
 
-    if len(sys.argv) > 2:
-        if sys.argv[2] == 'groq':
-            assistant = GroqAI()
-            assistant_name = "Groq"
-        else:
-            assistant = OllamaAI(model=MODEL)
-    else:
-        assistant = OllamaAI(model=MODEL)
+    # if len(sys.argv) > 2:
+    #     if sys.argv[2] == 'groq':
+    #         assistant = GroqAI()
+    #         assistant_name = "Groq"
+    #     else:
+    #         assistant = OllamaAI(model=MODEL)
+    # else:
+    #     assistant = OllamaAI(model=MODEL)
             
+    assistant = OllamaAI(model=MODEL)
+    
     sys.stderr.write(f"\n===\nUsing model: {MODEL} with assistant {assistant_name}\n===\n")
 
-    main(assistant, nerd_map)
+    # main(assistant, nerd_map)
+    readstd(procart)
+    
+    
     lap = perf_counter() - run_start_time
     lap = lap / 60
     sys.stderr.write(f"\nTotal Run time: {lap:.2f} min.\n")
